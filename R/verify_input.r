25661c1335183d882928b9cf519f934a2f16124f
rijanhastwoears7 <rijan_dhakal2055@protonmail.com>
2024-09-12 10:27:49 -0500

    Shifting to an input verification approach

--- R/verify_input.r
+++ R/verify_input.r
@@ -6,16 +6,22 @@ check_gwas_table <- function(current_gwas_table){
 
     needed_call_names <- c("CHROM","BP","Pvalues")
 
+    # check if the supplied table is a path or a table
+    if(is.character(current_gwas_table)){
+        current_gwas_table <- fread(current_gwas_table)
+    }
+
     # are all the fields present in the current table
     test_fields <- all(needed_call_names %in% colnames(current_gwas_table))
 
     if(!test_fields){
-        print("Your GWAS table either does not have the right data fields or has them mis-spelled.")
-
         print("You need to have these fields :- CHROM, BP and Pvalues.")
-
-        return(FALSE)
+        stop("The GWAS table does not have the right data fields or has them mis-spelled.")
+		stop()
     } else{
-        return(TRUE)
+        # If true select for the three fields that we need
+        current_gwas_table <- current_gwas_table %>%
+            select(CHROM, BP, Pvalues)
+        return(current_gwas_table)
     }
 }
