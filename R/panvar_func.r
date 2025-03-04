353da26715a6ce3df8d38c087e22f003263979d9
rijanhastwoears7 <rijan_dhakal2055@protonmail.com>
2025-03-04 10:35:15 -0600

    Fixes input validation issue
    https://github.com/danforthcenter/panvaR/issues/5 is fixed with this

--- R/panvar_func.r
+++ R/panvar_func.r
@@ -46,105 +46,113 @@
 #'
 #' @export
 panvar_func <- function(phenotype_data_path,vcf_file_path,tag_snps = NULL, r2_threshold = 0.6, maf = 0.05, missing_rate = 0.10, window = 500000,pc_min = 5,pc_max = 5, specific_pcs = NULL,dynamic_correlation = FALSE, all.impacts = FALSE){
-
-    # Check if the vcf_file has a tbi file
-    proper_tbi(vcf_file_path)
-
-    # The end goal of this function is to convieneintly make
-    # 1. The plot from Panvar
-    # 2. The report table -
-    # The report table should have - Gwas pvalues,
-
-    # Run GWAS for the user
-    gwas_table_denovo <- panvar_gwas(
-        phentotype_path = phenotype_data_path,
-        genotype_data = vcf_file_path,
-		specific_PCs = specific_pcs,
-        pc_min = pc_min,
-        pc_max = pc_max,
-        dynamic_correlation = dynamic_correlation,
-        maf = maf,
-        missing_rate = missing_rate
+  
+  if(!file.exists(vcf_file_path)){
+    stop("The genotype file path that you provided is not accessible. Either you supplied a wrong path or the file does not exist.")
+  }
+  
+  if(!file.exists(phenotype_data_path)){
+    stop("The phenotype file that you provided is not accessible. Either you supplied a wrong path or the file does not exist. ")
+  }
+  
+  # Check if the vcf_file has a tbi file
+  proper_tbi(vcf_file_path)
+  
+  # The end goal of this function is to convieneintly make
+  # 1. The plot from Panvar
+  # 2. The report table -
+  # The report table should have - Gwas pvalues,
+  
+  # Run GWAS for the user
+  gwas_table_denovo <- panvar_gwas(
+    phentotype_path = phenotype_data_path,
+    genotype_data = vcf_file_path,
+    specific_PCs = specific_pcs,
+    pc_min = pc_min,
+    pc_max = pc_max,
+    dynamic_correlation = dynamic_correlation,
+    maf = maf,
+    missing_rate = missing_rate
+  )
+  
+  # convert the window into bp values
+  window_bp <- window_unit_func(window)
+  
+  # The end goal of this function is to convieneintly make
+  # 1. The plot from Panvar
+  # 2. The report table -
+  # The report table should have - Gwas pvalues,
+  
+  # convert the vcf file to plink format
+  in_plink_format <- vcf_to_plink2(vcf_file_path)
+  
+  # clean up the supplied vcf file
+  cleaned_up <- bed_file_clean_up(in_plink_format$bed, maf = maf, missing_rate = missing_rate)
+  
+  gwas_table <- check_gwas_table(gwas_table_denovo)
+  # if the user has not supplied a tag SNP -
+  # get the tag snp from the gwas results
+  if(is.null(tag_snps)){
+    
+    denovo_tag_snp <- tag_snp_func(gwas_table_denovo)
+    
+    print("Note: you did not specify a tag snp - so the tag SNP will be inferred from the GWAS results")
+    
+    bp = denovo_tag_snp$tag_snp_bp
+    
+    chrom = denovo_tag_snp$tag_snp_chromosome
+    
+    panvar_result <- panvar_convienience_function( # Visit function body for documentation.
+      chrom = chrom,
+      bp = bp,
+      cleaned_up = cleaned_up,
+      vcf_file_path = vcf_file_path,
+      gwas_table = gwas_table,
+      in_plink_format = in_plink_format,
+      r2_threshold = r2_threshold,
+      window_bp = window_bp,
+      all.impacts = all.impacts
     )
-
-    # convert the window into bp values
-    window_bp <- window_unit_func(window)
-
-    # The end goal of this function is to convieneintly make
-    # 1. The plot from Panvar
-    # 2. The report table -
-    # The report table should have - Gwas pvalues,
-
-    # convert the vcf file to plink format
-    in_plink_format <- vcf_to_plink2(vcf_file_path)
-
-    # clean up the supplied vcf file
-    cleaned_up <- bed_file_clean_up(in_plink_format$bed, maf = maf, missing_rate = missing_rate)
-
-    gwas_table <- check_gwas_table(gwas_table_denovo)
-    # if the user has not supplied a tag SNP -
-    # get the tag snp from the gwas results
-    if(is.null(tag_snps)){
-
-        denovo_tag_snp <- tag_snp_func(gwas_table_denovo)
-
-        print("Note: you did not specify a tag snp - so the tag SNP will be inferred from the GWAS results")
-
-        bp = denovo_tag_snp$tag_snp_bp
-        
-        chrom = denovo_tag_snp$tag_snp_chromosome
-
-        panvar_result <- panvar_convienience_function( # Visit function body for documentation.
-            chrom = chrom,
-            bp = bp,
-            cleaned_up = cleaned_up,
-            vcf_file_path = vcf_file_path,
-            gwas_table = gwas_table,
-            in_plink_format = in_plink_format,
-            r2_threshold = r2_threshold,
-            window_bp = window_bp,
-            all.impacts = all.impacts
-        )
-
-    } else if(length(tag_snps) == 1){
-        user_tag_snp <- tag_snp_splitter(tag_snps)
-
-        chrom = user_tag_snp$chrom
-
-        bp = user_tag_snp$bp
-
-        panvar_result <- panvar_convienience_function( # Visit function body for documentation.
-            chrom = chrom,
-            bp = bp,
-            cleaned_up = cleaned_up,
-            vcf_file_path = vcf_file_path,
-            gwas_table = gwas_table,
-            in_plink_format = in_plink_format,
-            r2_threshold = r2_threshold,
-            window_bp = window_bp,
-            all.impacts = all.impacts
-        )
-
-    } else if(length(tag_snps) > 1){
-
-        list_of_tag_snps <- lapply(tag_snps, tag_snp_splitter)
-
-        panvar_result <- lapply(list_of_tag_snps, function(x){ # To split the list_of_tag_snps into their constituet parts.
-            panvar_convienience_function(# Visit function body for documentation.
-                chrom = x$chrom,
-                bp = x$bp,
-                cleaned_up = cleaned_up,
-                vcf_file_path = vcf_file_path,
-                gwas_table = gwas_table,
-                in_plink_format = in_plink_format,
-                r2_threshold = r2_threshold,
-                window_bp = window_bp,
-                all.impacts = all.impacts
-            )
-        })
-    }
-
-    return(panvar_result)
+    
+  } else if(length(tag_snps) == 1){
+    user_tag_snp <- tag_snp_splitter(tag_snps)
+    
+    chrom = user_tag_snp$chrom
+    
+    bp = user_tag_snp$bp
+    
+    panvar_result <- panvar_convienience_function( # Visit function body for documentation.
+      chrom = chrom,
+      bp = bp,
+      cleaned_up = cleaned_up,
+      vcf_file_path = vcf_file_path,
+      gwas_table = gwas_table,
+      in_plink_format = in_plink_format,
+      r2_threshold = r2_threshold,
+      window_bp = window_bp,
+      all.impacts = all.impacts
+    )
+    
+  } else if(length(tag_snps) > 1){
+    
+    list_of_tag_snps <- lapply(tag_snps, tag_snp_splitter)
+    
+    panvar_result <- lapply(list_of_tag_snps, function(x){ # To split the list_of_tag_snps into their constituet parts.
+      panvar_convienience_function(# Visit function body for documentation.
+        chrom = x$chrom,
+        bp = x$bp,
+        cleaned_up = cleaned_up,
+        vcf_file_path = vcf_file_path,
+        gwas_table = gwas_table,
+        in_plink_format = in_plink_format,
+        r2_threshold = r2_threshold,
+        window_bp = window_bp,
+        all.impacts = all.impacts
+      )
+    })
+  }
+  
+  return(panvar_result)
 }
 
 # panvar_convenience_function is an automation tool for the panvar logic, - 
@@ -164,98 +172,98 @@ panvar_convienience_function <- function(
     all.impacts = FALSE 
 )
 {
-    # subset your genotype data around the tag snp
-    subset_genotype_data <- subset_around_tag(cleaned_up,chrom = chrom, bp = bp, window = window_bp)
-
-    # using ld get the list of bps to keep
-    table <- ld_filtered_snp_list(subset_genotype_data,chrom = chrom, bp = bp, r2_threshold = r2_threshold)
-
-    # Make the LD table
-	# This table has the CHROM, BP and LD values and re-adds the tag SNP
-    # This is one of the tables that will be left_joined later
-    ld_table <- ld_table_maker(table)
-
-    # Convert the table into the list of SNPs to keep
-    # such that BCFtools can filter them out of a the original VCF file
-    keep_snp_list <- snps_to_keep(table)
-
-    # The way plink2 converts vcf files may lead to - 
-    # a situation where the names in plink2 -
-    # and BCFtools have different chromosomes names. -
-    # This needs to be accounted for.
-
-	plink2_bcf_dictionary <- plink2_bcftools_chroms_dictionary(vcf_file_path,in_plink_format$bim)
-
-	# If the dictionary is not empty then use it.
-	if(!is.null(plink2_bcf_dictionary)){
-		ld_table_checked <- apply_dict(plink2_bcf_dictionary, ld_table)
-
-		snp_keep_list_checked <- apply_dict(plink2_bcf_dictionary, keep_snp_list)
-
-        gwas_table_dicted <- apply_dict(plink2_bcf_dictionary, gwas_table)
-	} else{
-		
-		ld_table_checked <-  ld_table
-
-		snp_keep_list_checked <- keep_snp_list
-
-        gwas_table_dicted <- gwas_table
-	}
-	
-    # Sanitize the table 
-	# TODO:- Which table?
-    keep_table_path <- keep_table_sanitizer(snp_keep_list_checked)
-
-    # Send the keep table to bcftools
-    filtered_vcf_table <- filter_vcf_file(vcf_file_path = vcf_file_path, keep_table_path)
-
-    # split the SNP such that they have one annotation per line
-    split_table_path <- split_vcf_eff(filtered_vcf_table)
-
-    # Send the split table to SnpSift
-    # This returns a list that has a $path and -
-    # a $table
-    snpeff_table <- execute_snpsift(split_table_path)
-
-    # Read the output produced by SnpSift
-    snpsift_table <- snpeff_table$table
-
-    # We start the pipeline with a tag_SNP. By the time we reach the "final_table", - 
-    # we might not retain the tag_SNP if its impact factor is not HIGH or MODERATE. -
-    # The potential issue of the tag_SNP being dropped can be fixed by creating an -
-    # exception in the conditional here.
-
-    # For the final table, we create a new field that indicates if a loci is a Tag - 
-    # SNP or a Candidate gene. This will be recorded in TYPE.
+  # subset your genotype data around the tag snp
+  subset_genotype_data <- subset_around_tag(cleaned_up,chrom = chrom, bp = bp, window = window_bp)
+  
+  # using ld get the list of bps to keep
+  table <- ld_filtered_snp_list(subset_genotype_data,chrom = chrom, bp = bp, r2_threshold = r2_threshold)
+  
+  # Make the LD table
+  # This table has the CHROM, BP and LD values and re-adds the tag SNP
+  # This is one of the tables that will be left_joined later
+  ld_table <- ld_table_maker(table)
+  
+  # Convert the table into the list of SNPs to keep
+  # such that BCFtools can filter them out of a the original VCF file
+  keep_snp_list <- snps_to_keep(table)
+  
+  # The way plink2 converts vcf files may lead to - 
+  # a situation where the names in plink2 -
+  # and BCFtools have different chromosomes names. -
+  # This needs to be accounted for.
+  
+  plink2_bcf_dictionary <- plink2_bcftools_chroms_dictionary(vcf_file_path,in_plink_format$bim)
+  
+  # If the dictionary is not empty then use it.
+  if(!is.null(plink2_bcf_dictionary)){
+    ld_table_checked <- apply_dict(plink2_bcf_dictionary, ld_table)
     
-    if(all.impacts){
-        snpsift_table_impacts <- snpsift_table
-    } else {
-        snpsift_table_impacts <- snpsift_table %>% 
-            filter(IMPACT %in% c("HIGH","MODERATE") | BP == bp ) # The OR condition lets us retain the tag SNP which might be dropped if the IMPACT factor is not HIGH or MODERATE
-    }
-
-    # This table should have 
-    # 1. The impact factor
-    # 2. The tag SNP
-    # 3. The Pvalues from GWAS
-    # 4. The LD with the tag_snp included
-    pvalues_impact_ld_table <- snpsift_table_impacts %>%
-        left_join(gwas_table_dicted, by = c("CHROM","BP")) %>%
-        left_join(ld_table_checked, by = c("CHROM","BP"))
-
-    # Annotate the tag_snp
-    pvalues_impact_ld_colors_table <- pvalues_impact_ld_table %>% mutate(
-        Type = case_when(
-            BP == bp ~ "tag_snp",
-            BP != bp ~ "Candidate"
-        )
+    snp_keep_list_checked <- apply_dict(plink2_bcf_dictionary, keep_snp_list)
+    
+    gwas_table_dicted <- apply_dict(plink2_bcf_dictionary, gwas_table)
+  } else{
+    
+    ld_table_checked <-  ld_table
+    
+    snp_keep_list_checked <- keep_snp_list
+    
+    gwas_table_dicted <- gwas_table
+  }
+  
+  # Sanitize the table 
+  # TODO:- Which table?
+  keep_table_path <- keep_table_sanitizer(snp_keep_list_checked)
+  
+  # Send the keep table to bcftools
+  filtered_vcf_table <- filter_vcf_file(vcf_file_path = vcf_file_path, keep_table_path)
+  
+  # split the SNP such that they have one annotation per line
+  split_table_path <- split_vcf_eff(filtered_vcf_table)
+  
+  # Send the split table to SnpSift
+  # This returns a list that has a $path and -
+  # a $table
+  snpeff_table <- execute_snpsift(split_table_path)
+  
+  # Read the output produced by SnpSift
+  snpsift_table <- snpeff_table$table
+  
+  # We start the pipeline with a tag_SNP. By the time we reach the "final_table", - 
+  # we might not retain the tag_SNP if its impact factor is not HIGH or MODERATE. -
+  # The potential issue of the tag_SNP being dropped can be fixed by creating an -
+  # exception in the conditional here.
+  
+  # For the final table, we create a new field that indicates if a loci is a Tag - 
+  # SNP or a Candidate gene. This will be recorded in TYPE.
+  
+  if(all.impacts){
+    snpsift_table_impacts <- snpsift_table
+  } else {
+    snpsift_table_impacts <- snpsift_table %>% 
+      filter(IMPACT %in% c("HIGH","MODERATE") | BP == bp ) # The OR condition lets us retain the tag SNP which might be dropped if the IMPACT factor is not HIGH or MODERATE
+  }
+  
+  # This table should have 
+  # 1. The impact factor
+  # 2. The tag SNP
+  # 3. The Pvalues from GWAS
+  # 4. The LD with the tag_snp included
+  pvalues_impact_ld_table <- snpsift_table_impacts %>%
+    left_join(gwas_table_dicted, by = c("CHROM","BP")) %>%
+    left_join(ld_table_checked, by = c("CHROM","BP"))
+  
+  # Annotate the tag_snp
+  pvalues_impact_ld_colors_table <- pvalues_impact_ld_table %>% mutate(
+    Type = case_when(
+      BP == bp ~ "tag_snp",
+      BP != bp ~ "Candidate"
     )
-
-    # Call the weight function
-    final_reports_table <- overall_weight_func(pvalues_impact_ld_colors_table, bp = bp)
-
-    plot <- panvar_plot(final_reports_table, nrow(gwas_table))
-
-    return(list(plot = plot, table = final_reports_table))
+  )
+  
+  # Call the weight function
+  final_reports_table <- overall_weight_func(pvalues_impact_ld_colors_table, bp = bp)
+  
+  plot <- panvar_plot(final_reports_table, nrow(gwas_table))
+  
+  return(list(plot = plot, table = final_reports_table))
 }
