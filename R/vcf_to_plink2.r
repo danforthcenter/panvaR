fbf7669480c94e059a3aa11ce3f7b25eb8096d90
VectorFrantenstein <rijandhakal2055@gmail.com>
2025-03-25 10:02:20 -0500

    Better indentation using Rstudio

--- R/vcf_to_plink2.r
+++ R/vcf_to_plink2.r
@@ -23,86 +23,86 @@
 # A general function to convert vcf files to Plink
 
 vcf_to_plink2 <- function(vcf_file_path, output_prefix = NA){
-
-    # make a prefix for output
-    if(is.na(output_prefix)){
-
-		# tempoarary directory to store the files
-
-		plink2_tempdir <- temporary_directory()
-		
-        base_name_prefix <- 
-            base_name_func(vcf_file_path)
-
-		output_path = paste0(plink2_tempdir,"/",base_name_prefix)
-		
-    } else {
-        output_path <- output_prefix
+  
+  # make a prefix for output
+  if(is.na(output_prefix)){
+    
+    # tempoarary directory to store the files
+    
+    plink2_tempdir <- temporary_directory()
+    
+    base_name_prefix <- 
+      base_name_func(vcf_file_path)
+    
+    output_path = paste0(plink2_tempdir,"/",base_name_prefix)
+    
+  } else {
+    output_path <- output_prefix
+  }
+  # TODO :- If the user supplies a name copy the bed file from scratch to  
+  # create a temporary directory 
+  
+  
+  # check if bed and bim files already exist
+  bim_file_path = paste0(output_path,".bim")
+  bed_file_path = paste0(output_path,".bed")
+  
+  if(file.exists(bim_file_path) && file.exists(bed_file_path)){
+    # return the path to the bed file
+    print("It looks like your bed and bim files already exist. This was done with heuristics and regex.")
+    
+    print("The heuristics could be wrong, if that's the case please delete them and try again.")
+    
+    return(list(bed = bed_file_path,bim = bim_file_path))
+  }
+  binary_call <- "plink2"
+  
+  binary_args <- c(
+    "--allow-extra-chr",
+    "--vcf",
+    vcf_file_path, "--make-bed",
+    "--set-all-var-ids", "Chr_@_BP_#",
+    "--out", output_path
+  )
+  
+  # Rijan: Given how legible Plink2's error messages are
+  # Rijan: I think a simple STDOUT catch is good enough
+  
+  # Rijan: By the looks of it Plink2 can handle .gz and .vcf paths just fine
+  
+  tryCatch(
+    {
+      error_message <- tempfile()
+      try <- exec_wait(
+        binary_call,
+        args = binary_args,
+        std_out = TRUE,
+        std_err = error_message
+      )
+    },
+    error = function(e){
+      # Custom error message
+      print(paste("Execution attempt produced error:-", e$message))
+      1 # Return 1 on error
     }
-	# TODO :- If the user supplies a name copy the bed file from scratch to  
-	# create a temporary directory 
-
-
-    # check if bed and bim files already exist
-    bim_file_path = paste0(output_path,".bim")
+  )
+  
+  
+  
+  if(try == 0){
+    
     bed_file_path = paste0(output_path,".bed")
-
-    if(file.exists(bim_file_path) && file.exists(bed_file_path)){
-        # return the path to the bed file
-        print("It looks like your bed and bim files already exist. This was done with heuristics and regex.")
-
-        print("The heuristics could be wrong, if that's the case please delete them and try again.")
-
-        return(list(bed = bed_file_path,bim = bim_file_path))
-    }
-    binary_call <- "plink2"
-
-	binary_args <- c(
-        "--allow-extra-chr",
-		"--vcf",
-		vcf_file_path, "--make-bed",
-		"--set-all-var-ids", "Chr_@_BP_#",
-		"--out", output_path
-	)
-
-	# Rijan: Given how legible Plink2's error messages are
-	# Rijan: I think a simple STDOUT catch is good enough
-
-	# Rijan: By the looks of it Plink2 can handle .gz and .vcf paths just fine
-	
-    tryCatch(
-        {
-			error_message <- tempfile()
-            try <- exec_wait(
-                binary_call,
-                args = binary_args,
-                std_out = TRUE,
-                std_err = error_message
-            )
-        },
-        error = function(e){
-            # Custom error message
-            print(paste("Execution attempt produced error:-", e$message))
-            1 # Return 1 on error
-        }
+    
+    bim_file_path = paste0(output_path,".bim")
+    
+    print(
+      paste("The Plink files are available in",bed_file_path)
     )
-
-
-
-    if(try == 0){
-
-		bed_file_path = paste0(output_path,".bed")
-
-        bim_file_path = paste0(output_path,".bim")
-		
-        print(
-            paste("The Plink files are available in",bed_file_path)
-        )
-		return(list(bed = bed_file_path,bim = bim_file_path))
-    } else{
-
-		print("Your VCF file could not be converted to Plink2's native format.")
-		print("Please read the error message and re-try")
-		return(readLines(error_message))
-	}
+    return(list(bed = bed_file_path,bim = bim_file_path))
+  } else{
+    
+    print("Your VCF file could not be converted to Plink2's native format.")
+    print("Please read the error message and re-try")
+    return(readLines(error_message))
+  }
 }
--- R/verify_input.r
+++ R/verify_input.r
@@ -6,25 +6,25 @@
 # if not then stop, if yes then return the right subset
 
 check_gwas_table <- function(current_gwas_table){
-
-    needed_call_names <- c("CHROM","BP","Pvalues")
-
-    # check if the supplied table is a path or a table
-    if(is.character(current_gwas_table)){
-        current_gwas_table <- fread(current_gwas_table)
-    }
-
-    # are all the fields present in the current table
-    test_fields <- all(needed_call_names %in% colnames(current_gwas_table))
-
-    if(!test_fields){
-        print("You need to have these fields :- CHROM, BP and Pvalues.")
-        stop("The GWAS table does not have the right data fields or has them mis-spelled.")
-		stop()
-    } else{
-        # If true select for the three fields that we need
-        current_gwas_table <- current_gwas_table %>%
-            select(CHROM, BP, Pvalues)
-        return(current_gwas_table)
-    }
+  
+  needed_call_names <- c("CHROM","BP","Pvalues")
+  
+  # check if the supplied table is a path or a table
+  if(is.character(current_gwas_table)){
+    current_gwas_table <- fread(current_gwas_table)
+  }
+  
+  # are all the fields present in the current table
+  test_fields <- all(needed_call_names %in% colnames(current_gwas_table))
+  
+  if(!test_fields){
+    print("You need to have these fields :- CHROM, BP and Pvalues.")
+    stop("The GWAS table does not have the right data fields or has them mis-spelled.")
+    stop()
+  } else{
+    # If true select for the three fields that we need
+    current_gwas_table <- current_gwas_table %>%
+      select(CHROM, BP, Pvalues)
+    return(current_gwas_table)
+  }
 }
