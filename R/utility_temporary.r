b73d84dfb72ce18e8ceaf2643e3a6c6706666aa9
VectorFrantenstein <rijandhakal2055@gmail.com>
2025-03-25 06:52:53 -0500

    I have changed the name of the file and changed a couple of things about the way the function works. Previously the function randomly generated a name and there was no way for the user or other functions to supply a prefix to the function. A pre-fix for the file will make it possible to give the final file a better and more useful name.
    I have also added `utility_` to the name of the file
    itself. This is a new convention to make the code
    itself easier for the next person to maintain.

--- R/utility_temporary.r
+++ R/utility_temporary.r
@@ -28,13 +28,24 @@ temporary_directory <- function(delete_files = FALSE, working_directory = "panva
 # Function to generate a temporary file path in the specified directory
 # Optionally creates the file
 
-temp_file <- function(create_file = FALSE, working_directory = "panvar") {
+# `file_name`: Is the name of the intermediate file that is being given the -
+# the temporary name. For example, in a  run you will make many different -
+# temporary files, but more specifically different intermediate files serve -
+# different purposes. For example, you will have intermediate files 
+
+temp_file <- function(create_file = FALSE, working_directory = "panvar", file_name = NULL) {
   
   # Use provided working directory or default to "panvar"
   dir_path <- ifelse(is.null(working_directory), "panvar", working_directory)
   
-  # Generate a random file name
-  random_name <- paste0(sample(LETTERS, 5, replace = TRUE), collapse = "")
+  # Generate a random letters that you can use for the filename
+  random_letters <- paste0(sample(LETTERS, 5, replace = TRUE), collapse = "")
+
+  if(!is.null(file_name)){
+    random_name <- paste0(file_name,"_",random_letters)
+  } else{
+    random_name <- paste0(random_letters)
+  }
 
   # Create the final path
   final_path <- file.path(dir_path, random_name)
