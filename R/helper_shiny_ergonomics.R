8d3dcff7507c3d41578833aac2337dd62000253b
PoorRijan <rijandhakal2055@gmail.com>
2025-04-02 14:30:39 -0500

    This should solve the issue of panvar_gui

--- R/helper_shiny_ergonomics.R
+++ R/helper_shiny_ergonomics.R
@@ -1,6 +1,7 @@
-# Holds code that will make calling shiny app easier.
+# panvar_gui
 
 #' @export
+
 panvar_gui <- function(display.mode = "auto", launch.browser = TRUE, ...) {
   gui_code <- system.file("shiny", "panvar_shiny_main.R", package = "panvaR")
   shiny::runApp(gui_code, display.mode = display.mode, launch.browser = launch.browser, ...)
